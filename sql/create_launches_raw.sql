22ae362547ff1edd16004459c93546a26aefb6a5
Eldar Amir <eldaramir22@gmail.com>
2025-06-02 18:38:24 +0300

    fixed bug on sql raw

--- sql/create_launches_raw.sql
+++ sql/create_launches_raw.sql
@@ -1,9 +1,9 @@
-CREATE TABLE postgresql.public.launches_raw (
+CREATE TABLE spacex.public.launches_raw (
     id VARCHAR,
     name VARCHAR,
     date_utc TIMESTAMP,
     success BOOLEAN,
-    payload_mass_kg DOUBLE,
+    payload_mass_kg DOUBLE PRECISION,
     launchpad VARCHAR,
     rocket VARCHAR,
     date_unix BIGINT,
